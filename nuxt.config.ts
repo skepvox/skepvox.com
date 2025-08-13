64625677e29ce8d28e539b8719b9cacdcc259785
skepvox <skepvox@gmail.com>
2025-08-13 11:49:55 -0300

    Add modules ui and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/eslint'
+  ],
   nitro: {
     preset: 'vercel'
   }
